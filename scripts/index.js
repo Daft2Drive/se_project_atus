426e304b3c03e62462f55626540411b3f526aa31
Joshua Etienne <153865357+Daft2Drive@users.noreply.github.com>
2024-12-05 06:29:56 -0500

    Gear 6: Sun God Loki

--- scripts/index.js
+++ scripts/index.js
@@ -24,3 +24,70 @@ const initCards = [
         link:"https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg",
     },
 ];
+
+///*---------------------------------------------------------------------------------------------*///
+///*                                    Elements                                                 *///
+///*---------------------------------------------------------------------------------------------*///
+
+
+const profileEditButton = document.querySelector("#profile-profileEditButton");
+const profileEditModal = document.querySelector("#profile-edit-modal");
+const profileEditCloseButton = profileEditModal.querySelector(".modal__close");
+const profileTitle = document.querySelector(".profile__title");
+const profileDescription = document.querySelector(".profile__description");
+const profileTitleInput = document.querySelector("#profile-title-input");
+const profileDescriptionInput = document.querySelector("#profile-description-input");
+const profileEditForm = profileEditModal.querySelector("#modal-profile-form");
+const cardlistEl = document.querySelector(".cards__list");
+const cardTemplate = document.querySelector("#card-template").content.firstElementChild;
+
+
+///*---------------------------------------------------------------------------------------------*///
+///*                                    Functions                                                *///
+///*---------------------------------------------------------------------------------------------*///
+
+
+function closePopup() {
+    profileEditModal.classList.remove("modal_opened");
+}
+
+function getCardElement(data) {
+    const cardElement = cardTemplate.cloneNode(true);
+    const cardImageEl = cardElement.querySelector(".card__image");
+    const cardTitleEl = cardElement.querySelector(".card__title");
+
+    cardImageEl.src = data.link;
+    cardImageEl.alt = data.name;
+    cardTitleEl.textContent = data.name;
+    return cardElement;
+}
+
+///*---------------------------------------------------------------------------------------------*///
+///*                                    Event Handlers                                           *///
+///*---------------------------------------------------------------------------------------------*///
+
+function handleProfileEditSubmit (e) {
+    e.preventDefault();
+    profileTitle.textContent = profileTitleInput.value;
+    profileDescription.textContent = profileDescriptionInput.value;
+    closePopup();
+}
+
+///*---------------------------------------------------------------------------------------------*///
+///*                                    Event Listeners                                          *///
+///*---------------------------------------------------------------------------------------------*///
+
+profileEditButton.addEventListener("click", () => {
+    profileTitleInput.value = profileTitle.textContent;
+    profileDescriptionInput.value = profileDescription.textContent;
+    profileEditModal.classList.add("modal_opened");
+});
+
+profileEditCloseButton.addEventListener("click", closePopup);
+
+profileEditForm.addEventListener("submit", handleProfileEditSubmit);
+
+initCards.forEach((data) => {
+    const cardElement = getCardElement (data);
+    cardlistEl.prepend(cardElement);
+});

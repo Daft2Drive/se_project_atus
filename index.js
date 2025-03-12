6879dc95516e0c9f262f1fafe0129a3c7fe24162
Joshua Etienne <153865357+Daft2Drive@users.noreply.github.com>
2025-03-12 10:15:05 -0400

    Ragnir
    Zap

---
+++ index.js
@@ -0,0 +1,50 @@
+const initialCards = [
+    {
+        name: "Yosemite Valley",
+        link:"https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg",
+    },
+    {
+        name:"Lake Louise",
+        link:"https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lake-louise.jpg",
+    },
+    {
+        name:"Bald Mountain",
+        link:"https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/bald-mountains.jpg",
+    },
+    {
+        name:"Latemar",
+        link:"https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/latemar.jpg",
+    },
+    {
+        name:"Vanoise National Park",
+        link:"https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/vanoise.jpg",
+    },
+    {
+        name:"Lago di Braies",
+        link:"https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg",
+    },
+];
+
+///*---------------------------------------------------------------------------------------------*///
+///*                                    Elements                                                 *///
+///*---------------------------------------------------------------------------------------------*///
+
+// 
+const modal = document.querySelector('.modal');
+const profileCloseModal = document.querySelector('.modal__close');
+const profileEditModal = document.querySelector('.profile__edit-button');
+
+/*profileEditButton.addEventListener("click", () =>{
+    profileEditModal.classList.add("modal__opened");
+});*/
+
+function openModal() {
+    modal.classList.add('modal__opened');
+}
+
+function closeModal() {
+    modal.classList.remove('modal__opened');
+}
+
+profileEditModal.addEventListener("click", openModal);
+profileCloseModal.addEventListener("click", closeModal);
